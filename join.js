d5c7de2d99bd2c7478d3b8019808a73b80fac57d
wingyu200511 <wingyu.2005.11@gmail.com>
2026-04-20 19:17:33 +0800

    Add files via upload

---
+++ join.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('joinForm');
+
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      alert('申請已提交\n\n感謝你加入熱音俱樂部，我們會盡快與你聯絡！');
+
+      form.reset();
+    });
+  }
+});
